055f09e66259c66a69c96f6dae02aad5287cc35d
rupalisarcar <rupalisarcar@gmail.com>
2025-11-06 00:03:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded",(event)=>{
+	console.log("Dom Content");
+	const divText = document.createElement("div");
+	divText.textContent = "DOM load success"
+})
