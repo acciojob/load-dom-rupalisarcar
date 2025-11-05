ec23bfc1b614355cc67de4a3301edb6c1e62c9bb
rupalisarcar <rupalisarcar@gmail.com>
2025-11-06 00:27:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded",(event)=>{
 	console.log("Dom Content");
-	const divText = document.createElement("div");
-	divText.textContent = "DOM load success";
-	document.body.appendChild(divText)
+	if (document.body.children.length === 0) {
+        // Create a text node with the desired string
+        const textNode = document.createTextNode("DOM load success");
+        // Append the text node to the body
+        document.body.appendChild(textNode);
+    }
 })
